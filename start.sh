551ad3c068579f86f509cdfe173707dbc9df88fa
Silviu <silviu@localhost.localdomain>
2020-09-30 12:42:09 +0200

    done

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
- 
-### Openshift deploy script
-### Add version as parameter
-sudo podman run -it --privileged --name sensor-service localhost/quarkus/rest-client-jvm
-
-sudo podman inspect sensor-service | grep \"IPAddress\"
